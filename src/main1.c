5fed92e2e074e2a7d0be61f7a29f68bf33d6078f
PavelEgorov-ru <pkotofffff@yandex.ru>
2025-09-06 23:35:18 +0500

    шаг 3

---
+++ src/main1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int user_age = 35;
+    double pi = 3.14159;
+    char first_initial = 'S';
+
+    printf("Возраст пользователя %d лет\n", user_age);
+    printf("Приблизительное значение числа Пи %f\n", pi);
+    printf("Первая буква имени: %c\n", first_initial);
+
+    return 0;
+}
---
+++ src/step3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int user_age;
+    printf("введите ваш возраст\n");
+    scanf("%d", &user_age);
+    printf("Вам %d лет, спасибо!\n", user_age);
+    return 0;
+}
